c87959cdca7f2bf2bc7d90ba48e580cfe41e79df
André Walker <andre@andrewalker.net>
2012-10-11 10:44:32 -0300

    Add TODO

--- lib/CPI/Buyer.pm
+++ lib/CPI/Buyer.pm
@@ -12,6 +12,12 @@ has name => (
     is => 'ro',
 );
 
+# TODO
+# add all the other attrs.
+#
+# try and find the common ones between PagSeguro / PayPal / etc, and keep them
+# here. Specific attrs can stay in CPI::Buyer::${gateway}
+
 __PACKAGE__->meta->make_immutable;
 
 1;
