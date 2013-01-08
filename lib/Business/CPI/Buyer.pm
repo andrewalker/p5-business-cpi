a4989351b35d41412bba1df1528227d56b532269
André Walker <andre@andrewalker.net>
2013-01-08 10:09:53 -0200

    Add address information to the buyer

--- lib/Business/CPI/Buyer.pm
+++ lib/Business/CPI/Buyer.pm
@@ -1,5 +1,6 @@
 package Business::CPI::Buyer;
 use Moo;
+use Locale::Country ();
 
 # VERSION
 
@@ -15,6 +16,60 @@ has name => (
     is => 'ro',
 );
 
+has address_line1      => ( is => 'lazy' );
+has address_line2      => ( is => 'lazy' );
+
+has address_street     => ( is => 'ro', required => 0 );
+has address_number     => ( is => 'ro', required => 0 );
+has address_district   => ( is => 'ro', required => 0 );
+has address_complement => ( is => 'ro', required => 0 );
+has address_zip_code   => ( is => 'ro', required => 0 );
+has address_city       => ( is => 'ro', required => 0 );
+has address_state      => ( is => 'ro', required => 0 );
+has address_country    => (
+    is => 'ro',
+    required => 0,
+    isa => sub {
+        for (Locale::Country::all_country_codes()) {
+            return 1 if ($_ eq $_[0]);
+        }
+        for (Locale::Country::all_country_names()) {
+            return 1 if ($_ eq $_[0]);
+        }
+    },
+    coerce => sub {
+        for (Locale::Country::all_country_codes()) {
+            return $_ if ($_ eq $_[0]);
+        }
+        return Locale::Country::country2code($_[0]);
+    },
+);
+
+sub _build_address_line1 {
+    my $self = shift;
+
+    my $street = $self->address_street || '';
+    my $number = $self->address_number || '';
+
+    return unless $street;
+
+    return $street unless $number;
+
+    return "$street, $number";
+}
+
+sub _build_address_line2 {
+    my $self = shift;
+
+    my $distr = $self->address_district   || '';
+    my $compl = $self->address_complement || '';
+
+    return $distr if ($distr && !$compl);
+    return $compl if (!$distr && $compl);
+
+    return "$distr - $compl";
+}
+
 # TODO
 # add all the other attrs.
 #
@@ -27,7 +82,9 @@ __END__
 
 =head1 DESCRIPTION
 
-This class holds information about the buyer in a shopping cart.
+This class holds information about the buyer in a shopping cart. The address
+attributes are available so that if shipping is required, the buyer's address
+will be passed to the gateway (if the attributes were set).
 
 =attr email
 
@@ -36,3 +93,47 @@ Buyer's e-mail, which usually is their unique identifier in the gateway.
 =attr name
 
 Buyer's name.
+
+=attr address_line1
+
+=attr address_line2
+
+Some gateways (such as PayPal) do not define the street address as specific
+separate fields (such as Street, Number, District, etc). Instead, they only
+accept two address lines. For our purposes, we define a lazy builder for these
+attributes in case they are not directly set, using the specific fields
+mentioned above.
+
+=attr address_street
+
+Street name for shipping.
+
+=attr address_number
+
+Address number for shipping.
+
+=attr address_district
+
+District name.
+
+=attr address_complement
+
+If any extra information is required to find the address set this field.
+
+=attr address_zip_code
+
+Postal code.
+
+=attr address_city
+
+City.
+
+=attr address_state
+
+State.
+
+=attr address_country
+
+Locale::Country code for the country. You can set using the ISO 3166-1
+two-letter code, or the full name in English. It will coerce it and store the
+ISO 3166-1 two-letter code.
